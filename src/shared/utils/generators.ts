e92f324bbbaa1f2e08e6b744de1cee00f2268be3
mehmet-erim <bilgi@mehmeterim.com>
2019-09-13 22:40:06 +0300

    feature: add generator utils

---
+++ src/shared/utils/generators.ts
@@ -0,0 +1,15 @@
+export function generatePassword(length: number = 8) {
+  const charset =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let retVal = '';
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+export function uuid(a?: any): string {
+  return a
+    ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+    : ('' + 1e7 + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuid);
+}
